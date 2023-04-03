e67b8991a5aba8016d0bdbdebebd44364078fb0f
yingliufilms <yingliufilms@gmail.com>
2023-04-03 10:20:19 -0400

    created Container component to wrap Form and List; successfully performed search on Google Books api

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,2 +1,19 @@
-const Form = () => {};
+import React, { useState } from 'react';
+
+const Form = ({ onSubmit }) => {
+	const [value, setValue] = useState('');
+
+	return (
+		<form
+			onSubmit={e => {
+				e.preventDefault();
+				onSubmit(value);
+				setValue('');
+			}}
+		>
+			<input value={value} onChange={e => setValue(e.target.value)}></input>
+			<button type="submit">search</button>
+		</form>
+	);
+};
 export default Form;
--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,37 +1,9 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-const List = () => {
-	const [list, setList] = useState(null);
-	const searchPhrase = 'beautiful losers';
-
-	useEffect(() => {
-		fetch(
-			`https://books.googleapis.com/books/v1/volumes?q=${searchPhrase}&maxResults=3&key=${process.env.REACT_APP_API_KEY}`,
-			{
-				method: 'GET',
-				headers: { 'Content-Type': 'application/json' }
-			}
-		)
-			.then(res => {
-				if (res.status > 299 && res.status < 200) throw new Error();
-				return res.json();
-			})
-			.then(data => {
-				setList(
-					data.items.map(item => [
-						item.volumeInfo.title,
-						item.volumeInfo.categories?.[0],
-						item.volumeInfo.authors?.[0],
-						item.volumeInfo.description,
-						item.volumeInfo.imageLinks.smallThumbnail
-					])
-				);
-			});
-	}, []);
-
+const List = ({ lists }) => {
 	return (
 		<div>
-			{list.map((l, i) => (
+			{lists.map((l, i) => (
 				<li key={i}>{l}</li>
 			))}
 		</div>
